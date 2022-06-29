2fca283c17f37eac24daa5dd5bcab59bfb89a52a
Shubhangi <shubhangisingh453@gmail.com>
2022-06-29 09:44:52 +0530

    First commit

---
+++ app.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>app screen</title>
+</head>
+<body>
+   
+    <a href="logout.php"><button>logout</button></a>
+    
+    <h1>Hi! Guys welcome to website designed by Shubhangi<br>Hope you like this website<br>Trying to do my best...<br>Work in progress.....
+    <br>Thank You!</h1>
+    
+</body>
+</html>
